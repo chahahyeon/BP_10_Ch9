1343ad742ff57082fe70bfe1422b693ef84e0ee8
chahahyeon <112802649+chahahyeon@users.noreply.github.com>
2022-11-19 21:32:25 +0900

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -8,7 +8,7 @@ def draw_square(x, y, c):      #draw_square(x, y, c) 함수 선언
  t.up()                    #거북이가 이동할 때 선 생기지 않음
  t.goto(x, y)              #좌표 (x, y) 이동
  t.down()              #거북이 이동할 때 선 생김
- t.color("black",c)
+ t.color("black",c)       #펜은 "black", 채우기색은 c
  t.begin_fill()         #도형안에 색깔을 칠하기 위해 준비
  t.forward(100)          #100만큼 전진
  t.left(90)             #왼쪽으로 90도 회전
@@ -19,9 +19,9 @@ def draw_square(x, y, c):      #draw_square(x, y, c) 함수 선언
  t.forward(100)              #100만큼 전진
  t.left(90)                #왼쪽으로 90도 회전
  t.end_fill()                  #도형 안에 색깔 칠하고 종료
-for c in ["yellow", "red", "purple", "blue"]:
- x = random.randint(-100, 100)
- y = random.randint(-100, 100)
+for c in ["yellow", "red", "purple", "blue"]:    #변수 c를 "yellow", "red", "purple", "blue" 하나씩 사용
+ x = random.randint(-100, 100)    #x는 -100이상 100이하 범위의 정수 난수 생성
+ y = random.randint(-100, 100)    #y는 -100이상 100이하 범위의 정수 난수 생성
  draw_square(x, y, c)             # draw_square(x, y, c)함수 사용
  
 
@@ -38,20 +38,20 @@ def draw_shape(t, c, length, sides, x, y):         #draw_shape(t, c, length, sid
  t.up()                    #거북이가 이동할 때 선 생기지 않음
  t.goto(x, y)               #좌표 (x, y) 이동
  t.down()                 #거북이 이동할 때 선 생김
- t.fillcolor(c)
- angle = 360.0 / sides
+ t.fillcolor(c)         #채우기 색깔을 c로 변경
+ angle = 360.0 / sides     #angle(각도)는 360.0 / sides
  t.begin_fill()            #도형안에 색깔을 칠하기 위해 준비
- for dist in range(sides):
-  t.forward(length)
-  t.left(angle)
+ for dist in range(sides):    
+  t.forward(length)         #(length) 전진
+  t.left(angle)           #왼쪽으로 (angle) 회전
  t.end_fill()              #도형 안에 색깔 칠하고 종료
 
 for i in range(10):               #10번 반복
- color = random.choice([ 'white', 'yellow', 'blue', 'skyblue', 'orange', 'green' ])       #컬러는 랜덤으로 사용됨
- side_length = random.randint(10, 100)
- sides = random.randint(3, 10)
- x = random.randint(-200, 200)
- y = random.randint(-200, 200)
+ color = random.choice([ 'white', 'yellow', 'blue', 'skyblue', 'orange', 'green' ])       #컬러는 항목들 중에 랜덤으로 사용됨
+ side_length = random.randint(10, 100)           #side_length는 10이상 100이하 범위의 정수 난수 생성
+ sides = random.randint(3, 10)               #sides는 3이상 10이하 범위의 정수 난수 생성
+ x = random.randint(-200, 200)              #x는 -200이상 200이하 범위의 정수 난수 생성
+ y = random.randint(-200, 200)              #y는 -200이상 200이하 범위의 정수 난수 생성
  draw_shape(t, color, side_length, sides, x, y)            #draw_shape(t, c, length, sides, x, y)함수 사용
 
 
@@ -60,10 +60,10 @@ import turtle               #터틀 라이브러리를 가져옴
 import random              #랜덤 라이브러리를 가져옴
 t = turtle.Turtle()             #터틀 모듈에 터틀 함수를 t로 선언
 s = turtle.Screen()          #그림 그려지는 화면 얻음
-s.bgcolor("black")
+s.bgcolor("black")         #배경색 "black" 설정
 
 def draw_star(aturtle, colour, side_length, x, y):      #draw_star(aturtle, colour, side_length, x, y)함수 선언
- t.color(colour)
+ t.color(colour)           #(colour)색 사용
  t.begin_fill()            #도형안에 색깔을 칠하기 위해 준비
  t.penup()               #거북이가 이동할 때 선 생기지 않음
  t.goto(x, y)               #좌표 (x, y) 이동
@@ -75,8 +75,8 @@ def draw_star(aturtle, colour, side_length, x, y):      #draw_star(aturtle, colo
  t.end_fill()           #도형 안에 색깔 칠하고 종료
 
 for i in range(20):         #20번 반복
- color = random.choice([ 'white', 'yellow', 'blue', 'skyblue', 'orange', 'green' ])    #컬러는 랜덤으로 사용됨
- side_length = random.randint(10, 100)
- x = random.randint(-200, 200)
- y = random.randint(-200, 200)
+ color = random.choice([ 'white', 'yellow', 'blue', 'skyblue', 'orange', 'green' ])    #컬러는 항목들 중에 랜덤으로 사용됨
+ side_length = random.randint(10, 100)            #side_length는 10이상 100이하 범위의 정수 난수 생성
+ x = random.randint(-200, 200)                 #x는 -200이상 200이하 범위의 정수 난수 생성
+ y = random.randint(-200, 200)                 #y는 -200이상 200이하 범위의 정수 난수 생성
  draw_star(t, color, side_length, x, y)          #draw_star(aturtle, colour, side_length, x, y)함수 사용
